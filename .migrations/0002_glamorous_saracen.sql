32d82fe49e24a251fbf997077532b50c1d7ed83e
Matheus Teixeira <149537799+Matheuscrf@users.noreply.github.com>
2024-12-09 16:15:02 -0300

    consertando erro

--- .migrations/0002_glamorous_saracen.sql
+++ .migrations/0002_glamorous_saracen.sql
@@ -1,1 +1,6 @@
-ALTER TABLE "goals" RENAME COLUMN "desired_weekly_0frequency" TO "desired_weekly_frequency";
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='goals' AND column_name='desired_weekly_0frequency') THEN
+        ALTER TABLE "goals" RENAME COLUMN "desired_weekly_0frequency" TO "desired_weekly_frequency";
+    END IF;
+END $$;
